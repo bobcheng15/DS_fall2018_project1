35d66d69dde3f61ac964123691e91acf554d62c2
Bob Cheng <bobcheng15@gmail.com>
2018-10-10 00:00:26 +0800

    read_matrix function completed

--- yourproject.cpp
+++ yourproject.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+void read_matrix(std::ifstream & input_file, const int & column, int * matrix){
+    for (int i = 0; i < 3; i ++){
+        for (int j = 0; j < column; j ++){
+            input_file >> matrix[i * column + j];
+        }
+    }
+}
 int main(void){
-    std::ifstream file;
-    std::string output;
-    file.open("example.txt");
-    file >> output;
-    std::cout << output << '\n';
-    file.close();
-    return 0;
+    int row, column;
+    std::ifstream input_file;
+    input_file.open("matrix.data");
+    input_file >> row;
+    input_file >> column;
+    std::cout << row << " " << column << '\n';
+    int matrix [3][column];
+    read_matrix(input_file, column, &matrix[0][0]);
+    for (int i = 0; i < 3; i ++){
+        for (int j = 0; j < column; j ++){
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
 }
