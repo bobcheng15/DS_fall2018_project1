1dc84d5e67845e0c76332e5b464ccc018d4325d1
Bob Cheng <bobcheng15@gmail.com>
2018-10-10 02:06:08 +0800

    commiting again because previous commit somehow failed

--- yourproject.cpp
+++ yourproject.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-void read_matrix(std::ifstream & input_file, const int & column, int * matrix){
+void read_matrix(std::ifstream & input_file, const int & column, int * matrix, int & rotating_row){
     static bool first = true;
     if (first){
         for (int i = 0; i < 3; i ++){
@@ -12,25 +12,23 @@ void read_matrix(std::ifstream & input_file, const int & column, int * matrix){
         first = false;
     }
     else{
-        for (int i = 0; i < 2; i ++){
-            for (int j = 0; j < column; j ++){
-                matrix[i * column + j] = matrix[(i + 1) * column + j];
-            }
-        }
         for (int i = 0; i < column; i ++){
-            input_file >> matrix[2 * column + i];
+            input_file >> matrix[rotating_row * column + i];
         }
+        rotating_row = rotating_row + 1;
     }
 }
 int main(void){
     int row, column;
+    int rotating_row = 0;
     std::ifstream input_file;
     input_file.open("matrix.data");
     input_file >> row;
     input_file >> column;
+    std::cout << row << " " << column << '\n';
     int matrix [3][column];
     for (int num = 0; num < row - 2; num ++){
-        read_matrix(input_file, column, &matrix[0][0]);
+        read_matrix(input_file, column, &matrix[0][0], rotating_row);
         for (int i = 0; i < 3; i ++){
             for (int j = 0; j < column; j ++){
                 std::cout << matrix[i][j] << " ";
